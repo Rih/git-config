0abc9b4f9854f8b10429dd16bcbc1bff4480f2cf
rodrihgo <drigox90rih@gmail.com>
2018-10-22 15:12:59 -0300

    config

---
+++ git-client.sh
@@ -0,0 +1,41 @@
+!#/bin/bash
+
+$serverIP="192.168.11.11"
+$clientUser="clientName"
+$serverUser="git"
+$serverProject="project-example"
+$newProject="project"
+$serverPath="/home/${serverUser}"
+$serverProjectPath="${serverPath}/projects"
+$clientPath="/home/${clientUser}"
+
+## On Client
+echo "Creating a new client SSH KEY..."
+ssh-keygen -t rsa
+
+echo "Sending public key to Remote Git Server and adding to authorizeds..."
+cat ${clientPath}/.ssh/id_rsa.pub | ssh ${serverUser}@${serverIP} "mkdir -p ${serverPath}/.ssh && cat >> ${serverPath}/.ssh/authorized_keys"
+
+# On Client
+echo "Creating a new project path..."
+mkdir -p ~/projects
+
+cd ~/projects
+
+# DO ONE OF TWO
+#git init 
+#git add .
+#git commit -m "first"
+# After this, DO remote add
+#Otherwise DO :Clone:
+
+#-------
+# :Clone:
+echo "Cloning ... (git@${serverIP}:${serverProjectPath}/${serverProject}.git ${newProject})"
+git clone git@${serverIP}:${serverProjectPath}/${serverProject}.git ${newProject}
+
+# :remote add:
+echo "Linking local repo from remote server for synchronization..."
+echo "(ssh://${serverUser}@${serverIP}${serverProjectPath}/${serverProject}.git)"
+git remote add origin ssh://${serverUser}@${serverIP}${serverProjectPath}/${serverProject}.git
+echo "DONE."
---
+++ git-server.sh
@@ -0,0 +1,42 @@
+!#/bin/bash
+
+$serverIP="192.168.11.11"
+$serverUser="git"
+$newProject="project-example"
+$serverPath="/home/${serverUser}"
+## On Server
+# Run once
+# sudo apt-get update
+# Run once
+# sudo apt-get install -y git-core
+echo "Creating new Git server user..."
+sudo useradd ${serverUser}
+echo "Creating passwd to ${serverUser}..."
+passwd ${serverUser}
+
+## On Server
+echo "Creating path to the project in ${serverPath}/projects/${newProject}.git"
+mkdir -p ${serverPath}/projects/${newProject}.git
+
+cd ${serverPath}/projects/${newProject}.git
+
+echo "Creating an empty repo..."
+git init --bare
+echo "DONE."
+
+
+
+
+# Gitlab installation
+
+#sudo apt-get install postfix
+#
+#sudo dpkg-reconfigure postfix
+#
+#wget https://downloads-packages.s3.amazonaws.com/ubuntu-14.04/gitlab_7.9.4-omnibus.1-1_amd64.deb
+#
+#sudo dpkg -i gitlab_7.9.4-omnibus.1-1_amd64.deb
+#
+#sudo gitlab-ctl reconfigure
+#
+#vim /etc/gitlab/gitlab.rb
